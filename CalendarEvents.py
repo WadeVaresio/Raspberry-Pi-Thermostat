2ab052dffb3ef6584a479e9ab30b93c24890ad18
Wade Varesio <wadevaresio@gmail.com>
2018-11-01 11:13:32 -0700

    Made integration with google calendar into its own class to be imported

---
+++ CalendarEvents.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+import datetime
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+class CalendarEvents:
+    SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+
+    def __init__(self, credential_name):
+        self.credential_name = credential_name
+        self.setup_api()
+
+
+    def setup_api(self):
+        self.store = file.Storage("token.json")
+        self.credentials = self.store.get()
+
+        if not self.credentials or self.credentials.invalid:
+            flow = client.flow_from_clientsecrets(self.credentialName, self.SCOPES)
+            self.credentials = tools.run_flow(flow, self.store)
+
+        self.service = build('calendar', 'v3', http=self.credentials.authorize(Http()))
+
+    def get_events(self, num_events):
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+
+        events_result = self.service.events().list(calendarId='primary', timeMin=now,
+                                              maxResults=num_events, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print("there are no upcoming events in the calendar")
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            #print(start, event['summary'])
+
+        return events
--- main.py
+++ main.py
@@ -1,37 +1,9 @@
-from __future__ import print_function
-import datetime
-from googleapiclient.discovery import build
-from httplib2 import Http
-from oauth2client import file, client, tools
+import CalendarEvents
 
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
-
-def setup_api():
-    store = file.Storage("token.json")
-    credentials = store.get()
-
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-        credentials = tools.run_flow(flow, store)
-
-    global service
-    service = build('calendar', 'v3', http=credentials.authorize(Http()))
-
-def get_events(numEvents):
-    global upcoming_events
-    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                          maxResults=numEvents, singleEvents=True,
-                                          orderBy='startTime').execute()
-    events = events_result.get('items', [])
-
-    if not events:
-        print("there are no upcoming events in the calendar")
-    for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+google_cal = CalendarEvents.CalendarEvents('credentials.json')
 
 if __name__ == "__main__":
-    setup_api()
-    get_events(10)
+    events = google_cal.get_events(10)
+
+    for event in events:
+        print(event['summary'])
