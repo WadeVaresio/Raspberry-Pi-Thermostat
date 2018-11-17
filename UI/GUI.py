3a82b3a25c36dcfc9fe0f992442ebd961ce5f02f
Wade Varesio <wadevaresio@gmail.com>
2018-11-16 21:27:09 -0800

    Cleanup, small UI additions. Preparing for UI update.

--- UI/GUI.py
+++ UI/GUI.py
@@ -2,7 +2,6 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
-from kivy.clock import Clock
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.image import Image
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -15,22 +14,37 @@ Builder.load_file('UI/ImageButton.kv')
 Window.clearcolor = (1,1,1,1)
 Window.size = (1000, 1000)
 
+
+# TODO Implement Kivy Google Material Theme
 class Main(App):
+    def update_time(self):
+        screen_manager.get_screen('main').ids.clock_label.text = time.asctime()
+
     def build(self):
-        Clock.schedule_interval(MainScreen.update_time, 1)
+        update_time_scheduler = BackgroundScheduler()
+        update_time_scheduler.add_job(self.update_time, 'interval', seconds=0.9)
+
+        try:
+            update_time_scheduler.start()
+        except KeyboardInterrupt:
+            update_time_scheduler.shutdown()
+
         return screen_manager
 
 
 class MainScreen(Screen):
-    def exitProgram(self):
+    def exit_program(self):
         quit()
 
-    def update_time(self, *args):
-        screen_manager.get_screen('main').ids.clock_label.text = time.asctime() #Needed since method is accessed from Main Class
+    def update_time(self):
+        screen_manager.get_screen('main').ids.clock_label.text = time.asctime()
 
-    def update_current_temp(self):
+    def set_button_press(self):
+        pass
 
-        screen_manager.get_screen('main').ids.current_temp_label.text = "Current temperature" # TODO write the code to fetch the current tempreature
+    def see_upcoming_events(self):
+        screen_manager.current = 'upcoming_events'
+        print("upcoming events")
 
 
 class ImageButton(ButtonBehavior, Image):
@@ -38,15 +52,24 @@ class ImageButton(ButtonBehavior, Image):
         # TODO implement increase temperature
         print("arrow up imagebutton pressed")
 
-    def decrease_temeprature(self):
+    def decrease_temperature(self):
         # TODO implement decrease temperature
         print("arrow down imagebutton pressed")
 
-screen_manager.add_widget(MainScreen(name = 'main'))
+
+class UpcomingEvents(Screen):
+    pass
+
+
+screen_manager.add_widget(MainScreen(name='main'))
+screen_manager.add_widget(UpcomingEvents(name='upcoming_events'))
+
 
 def initialize():
-    refresh_temperature_scheduler = BackgroundScheduler()
-    #refresh_temperature_scheduler.add_job(MainScreen.update_current_temp(), 'interval', seconds=10)
-
     m = Main()
     m.run()
+
+
+def update_temperature():
+    # TODO implement temperature sensor
+    screen_manager.get_screen('main').ids.current_temp_label.text = "Current Temp\n"
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from CalendarEvents import CalendarEvents
-from UI import GUI # TODO consider making a python module to hold all files to be imported
+from UI import GUI  # TODO consider making a python module to hold all files to be imported
 from apscheduler.schedulers.background import BackgroundScheduler
+import sys
 
 google_cal = CalendarEvents.CalendarEvents('CalendarEvents/credentials.json')
 calendar_events = []
+
 
 def refresh_calendar_events():
     fetched_events = google_cal.get_events(10)
@@ -11,26 +13,36 @@ def refresh_calendar_events():
         print(event['summary'])
     check_for_new_events(fetched_events)
 
+
 def check_for_new_events(fetched_events):
     global calendar_events
     new_event = False
 
     for fetched_event in fetched_events:
-       for existing_event in calendar_events:
-           if fetched_event is not existing_event:
-               new_event = True
-               calendar_events = fetched_events
-               return new_event
+        for existing_event in calendar_events:
+            if fetched_event is not existing_event:
+                new_event = True
+                calendar_events = fetched_events
+                return new_event
     return new_event
+
+
+def update_temperature():
+    GUI.update_temperature()
 
 
 if __name__ == "__main__":
     refresh_events_scheduler = BackgroundScheduler()
-    refresh_events_scheduler.add_job(refresh_calendar_events, 'interval', seconds=10)
+    refresh_events_scheduler.add_job(refresh_calendar_events, 'interval', minutes=1)
+
+    update_current_temp_scheduler = BackgroundScheduler()
+    update_current_temp_scheduler.add_job(update_temperature, 'interval', seconds=1)
 
     try:
         refresh_events_scheduler.start()
+        update_current_temp_scheduler.start()
     except (KeyboardInterrupt, SystemExit):
         refresh_events_scheduler.shutdown()
+        update_current_temp_scheduler.shutdown()
 
     GUI.initialize()
