bbe63175b036701bbdea7ca5d63fc09f028a3aff
Wade Varesio <wadevaresio@gmail.com>
2019-01-06 16:58:04 -0800

    Implemented queue for database writes, implementation of Database now allows for use without creating an object fixing issue of multi thread references

--- main.py
+++ main.py
@@ -3,19 +3,19 @@
 from thermostat.calendarevents import GoogleCalendar
 from thermostat.ui import GUI
 from apscheduler.schedulers.background import BackgroundScheduler
+from thermostat.database import Database
 
 google_cal = GoogleCalendar('credentials.json')
 MAX_AMOUNT_OF_EVENTS = 10
 
 
-
 def refresh_calendar_events():
     """
-    Refresh the calendar events, for use with BackgroundScheduler
+    Refresh the calendar events and write any new events in the database
     :return: None
     """
-    #TODO implement with database
-    pass
+    Database.add_to_queue(google_cal.get_events(MAX_AMOUNT_OF_EVENTS))
+    Database.write_from_queue()
 
 
 def update_temperature():
@@ -24,7 +24,7 @@ def update_temperature():
 
 if __name__ == "__main__":
     refresh_events_scheduler = BackgroundScheduler()
-    refresh_events_scheduler.add_job(refresh_calendar_events, 'interval', minutes=1)
+    refresh_events_scheduler.add_job(refresh_calendar_events, 'interval', seconds=10)
 
     update_current_temp_scheduler = BackgroundScheduler()
     update_current_temp_scheduler.add_job(update_temperature, 'interval', minutes=5)
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         refresh_events_scheduler.start()
         update_current_temp_scheduler.start()
         update_temperature()
-        #GUI.initialize()
+        GUI.initialize()
     except (KeyboardInterrupt, SystemExit):
         refresh_events_scheduler.shutdown()
         update_current_temp_scheduler.shutdown()
--- thermostat/database/Database.py
+++ thermostat/database/Database.py
@@ -1,58 +1,134 @@
 import sqlite3
+import sys
+if sys.version_info[0] != 3:
+    sys.exit("This script requires Python 3")
+from queue import Queue
+
+QUEUE = Queue()
+DATABASE_FILE = 'database'
 
 
-class Database:
+def write_event(ID, start, end, temperature, creator):
+    """
+    Write an event to the database, checks if it exists already
+    :param ID: ID of the event
+    :param start: start time of the event
+    :param end: end time of the event
+    :param temperature: corresponding temperature
+    :param creator: creator of the event (Name from the google account)
+    :return: None
+    """
+    database = connect_to_database()
 
-    def __init__(self, file_path):
-        """
-        Constructs a new database object responsible for communicating with the SQLite database
-        :param file_path: File path to the database file
-        :return: New Database object
-        """
-        if not isinstance(file_path, str):
-            raise Exception("The filepath specified is invalid")
+    if check_for_existing_event(ID):
+        update_event(ID, start, end, temperature, creator)
+        return
 
-        self.database = sqlite3.connect(file_path)
-
-    def write_event(self, id, date, temperature, creator):
-        """
-        Write an event to the database.
-        :param id: ID for the data entry
-        :param date: Date of the event
-        :param temperature: Temperature corresponding to the calendar event
-        :param creator: Creator of the event
-        :return:
-        """
-        self.database.execute("INSERT INTO EVENTS(ID, Date, Temperature, Creator) VALUES (?, ? ,?, ?)", [
-                id, date, temperature, creator])
-        self.database.commit()
-
-    def remove_event_by_id(self, id):
-        """
-        Remove an event based on it's ID
-        :param id: ID of the event to be removed
-        :return: None
-        """
-        self.database.execute("DELETE from EVENTS where ID = ?;", [id])
-        self.database.commit()
+    database.execute("INSERT INTO EVENTS(ID, StartTime, EndTime, Temperature, Creator) VALUES (?, ? ,?, ?, ?)", [
+        ID, start, end, temperature, creator])
+    database.commit()
 
 
-    def print_all_events(self):
-        """
-        Prints all of the database events used for debugging purposes
-        :return: None
-        """
-        cursor = self.database.execute("SELECT ID, Date, Temperature, Creator from EVENTS")
+def remove_event_by_id(ID):
+    """
+    Remove an event based on it's ID
+    :param ID: ID of the event to be removed
+    :return: None
+    """
+    # TODO verify this method is working
+    database = connect_to_database()
+    database.execute("DELETE from EVENTS where ID = ?;", [ID])
+    database.commit()
 
-        for row in cursor:
-            print("ID: " + str(row[0]))
-            print("Date: " + row[1])
-            print("Temperature: " + str(row[2]))
-            print("Creator: " + row[3])
 
-    def close_database(self):
-        """
-        Close the connection to the database
-        :return: None
-        """
-        self.database.close()
+def print_all_events():
+    """
+    Prints all of the database events used for debugging purposes
+    :return: None
+    """
+    database = connect_to_database()
+    cursor = database.execute("SELECT ID, StartTime, EndTime, Temperature, Creator from EVENTS")
+
+    for row in cursor:
+        print("ID: " + str(row[0]))
+        print("Start Time: " + row[1])
+        print("End Time: " + row[2])
+        print("Temperature: " + str(row[3]))
+        print("Creator: " + row[4])
+
+
+def close_database(self):
+    """
+    Close the connection to the database
+    :return: None
+    """
+    self.database.close()
+
+
+def add_to_queue(data):
+    global QUEUE
+    QUEUE.put(data)
+
+
+def write_from_queue():
+    for index in range(QUEUE.qsize()):
+        events = QUEUE.get()
+        for event in events:
+            ID = event['id']
+            start = event['start'].get('dateTime')
+            end = event['end'].get('dateTime')
+            temp = float(event['summary'])
+            creator = event['creator'].get('displayName')
+            write_event(ID, start, end, temp, creator)
+
+
+def get_all_events():
+    """
+    Get all of the events stored in the database
+    :return: All data from the EVENTS table in the database
+    """
+    database = connect_to_database()
+    data = database.execute("SELECT ID, StartTime, EndTime, Temperature, Creator FROM EVENTS")
+    return data
+
+
+def check_for_existing_event(event_id):
+    """
+    Checks if existing event with the same ID exists
+    :param event_id: ID of the event to check the database against
+    :return: True if there is an existing event in the database with the same ID
+    """
+    database = connect_to_database()
+    data = database.execute("SELECT ID from EVENTS")
+
+    for row in data:
+        if event_id == str(row[0]):
+            return True
+    return False
+
+
+def update_event(ID, start, end, temperature, creator):
+    """
+    Update an event in the database based off its ID
+    :param ID: ID of the event you want to change
+    :param start: start time to update the database entry
+    :param end: end time to update the database entry
+    :param temperature: temperature to update the database entry
+    :param creator: creator to update the database entry
+    :return: None
+    """
+    database = connect_to_database()
+    database.execute("UPDATE EVENTS SET StartTime = ? WHERE ID = ?", [start, ID])
+    database.execute("UPDATE EVENTS SET EndTime = ? WHERE ID = ?", [end, ID])
+    database.execute("UPDATE EVENTS SET Temperature = ? WHERE ID = ?", [temperature, ID])
+    database.execute("UPDATE EVENTS SET Creator = ? WHERE ID = ?", [creator, ID])
+    database.commit()
+
+
+def connect_to_database():
+    """
+    Connect to the SQLite database
+    :return: SQLite3 object
+    """
+    return sqlite3.connect(DATABASE_FILE)
+
